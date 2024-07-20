a24bfb763367cac27d3bbcf6192a4438e825b3ee
Billy C. <clercbilly@gmail.com>
2024-07-20 10:06:01 +0200

    Added logging library

--- Atlas/src/Atlas/EntryPoint.h
+++ Atlas/src/Atlas/EntryPoint.h
@@ -6,6 +6,11 @@ extern Atlas::Application* Atlas::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Atlas::Log::Init();
+	ATLAS_CORE_WARN("Initialized Log!");
+	int a = 5;
+	ATLAS_INFO("Initialized Log {0}!", a);
+
 	auto app = Atlas::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Atlas/src/Atlas/Log.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Atlas
+{
+	class ATLAS_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// Core log macros
+#define ATLAS_CORE_TRACE(...)   ::Atlas::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define ATLAS_CORE_INFO(...)    ::Atlas::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define ATLAS_CORE_WARN(...)    ::Atlas::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define ATLAS_CORE_ERROR(...)   ::Atlas::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define ATLAS_CORE_FATAL(...)   ::Atlas::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log macros
+#define ATLAS_TRACE(...)        ::Atlas::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define ATLAS_INFO(...)         ::Atlas::Log::GetClientLogger()->info(__VA_ARGS__)
+#define ATLAS_WARN(...)         ::Atlas::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define ATLAS_ERROR(...)        ::Atlas::Log::GetClientLogger()->error(__VA_ARGS__)
+#define ATLAS_FATAL(...)        ::Atlas::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
+
