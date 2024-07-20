e4e598c9d7cf31f0f2aa41f00f83b0d763fd2fa2
Billy C. <clercbilly@gmail.com>
2024-07-20 07:55:08 +0200

    Project setup
    Application and entry point setup.

---
+++ Atlas/src/Atlas.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// For use by Atlas applications
+
+#include "Atlas/Application.h"
+
+/* ----- ENTRY POINT ----- */
+#include "Atlas/EntryPoint.h"
---
+++ Atlas/src/Atlas/Core.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef ATLAS_PLATFORM_WINDOWS
+	#ifdef ATLAS_BUILD_DLL
+		#define ATLAS_API __declspec(dllexport)
+	#else
+		#define ATLAS_API __declspec(dllimport)
+	#endif
+#else
+	#error Atlas only supports Windows
+#endif
