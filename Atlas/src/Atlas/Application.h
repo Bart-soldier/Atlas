e3a0f608eb31222c5b8c6719184e19f7f6d201c7
Billy C. <clercbilly@gmail.com>
2024-08-02 07:13:45 +0200

    Started implementing the RendererAPI

--- Atlas/src/Atlas/Application.h
+++ Atlas/src/Atlas/Application.h
@@ -10,6 +10,7 @@
 #include "Atlas/ImGui/ImGuiLayer.h"
 
 #include "Atlas/Renderer/Shader.h"
+#include "Atlas/Renderer/Buffer.h"
 
 namespace Atlas
 {
@@ -37,8 +38,10 @@ namespace Atlas
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 
 		static Application* s_Instance;
 	};
---
+++ Atlas/src/Atlas/Renderer/Buffer.cpp
@@ -0,0 +1,39 @@
+#include "atlaspch.h"
+#include "buffer.h"
+
+#include "Renderer.h"
+
+#include "Platform/OpenGL/OpenGLBuffer.h"
+
+namespace Atlas
+{
+	VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::None:
+				ATLAS_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+				return nullptr;
+			case RendererAPI::OpenGL:
+				return new OpenGLVertexBuffer(vertices, size);
+		}
+
+		ATLAS_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
+	IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t count)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::None:
+				ATLAS_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+				return nullptr;
+			case RendererAPI::OpenGL:
+				return new OpenGLIndexBuffer(indices, count);
+		}
+
+		ATLAS_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+}
