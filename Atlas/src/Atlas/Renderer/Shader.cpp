c4a5b51849e6bc2894c30ed69656b72fcfbb3545
Billy C. <clercbilly@gmail.com>
2024-08-19 08:02:57 +0200

    Now able to read shaders from files

--- Atlas/src/Atlas/Renderer/Shader.cpp
+++ Atlas/src/Atlas/Renderer/Shader.cpp
@@ -6,6 +6,21 @@
 
 namespace Atlas
 {
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			ATLAS_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+			return nullptr;
+		case RendererAPI::API::OpenGL:
+			return new OpenGLShader(filepath);
+		}
+
+		ATLAS_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
