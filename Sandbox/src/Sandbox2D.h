e923835b62d21e7133117085342b175240c06779
Billy C. <clercbilly@gmail.com>
2024-08-26 08:01:28 +0200

    Added Textured Quad

--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -21,6 +21,8 @@ private:
 	Atlas::Ref<Atlas::VertexArray> m_SquareVA;
 	Atlas::Ref<Atlas::Shader> m_FlatColorShader;
 
+	Atlas::Ref<Atlas::Texture2D> m_CheckerboardTexture;
+
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 };
 
