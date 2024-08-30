1236c1833441c6cddb0a6dcd2f43a041b2734460
Billy C. <clercbilly@gmail.com>
2024-08-30 08:22:53 +0200

    Changed ExampleLayer to Sandbox3D

--- Atlas/src/Platform/OpenGL/OpenGLShader.cpp
+++ Atlas/src/Platform/OpenGL/OpenGLShader.cpp
@@ -61,10 +61,18 @@ namespace Atlas
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
-			result.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
-			in.close();
+			size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(in.tellg());
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], result.size());
+				in.close();
+			}
+			else
+			{
+				ATLAS_CORE_ERROR("Could not read file '{0}'", filepath);
+			}
 		}
 		else
 		{
