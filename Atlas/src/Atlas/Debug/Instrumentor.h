31a9fce1ad014c55fbcd8780f95454907c7a8c22
Billy C. <clercbilly@gmail.com>
2024-08-31 07:17:04 +0200

    Better macros for Instrumentor

--- Atlas/src/Atlas/Debug/Instrumentor.h
+++ Atlas/src/Atlas/Debug/Instrumentor.h
@@ -160,10 +160,31 @@ namespace Atlas
 
 #define ATLAS_PROFILE 1
 #if ATLAS_PROFILE
+	// Resolve which function signature macro will be used. Note that this only
+	// is resolved when the (pre)compiler starts, so the syntax highlighting
+	// could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define ATLAS_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define ATLAS_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define ATLAS_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define ATLAS_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define ATLAS_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define ATLAS_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define ATLAS_FUNC_SIG __func__
+	#else
+		#define ATLAS_FUNC_SIG "ATLAS_FUNC_SIG unknown!"
+	#endif
+
 	#define ATLAS_PROFILE_BEGIN_SESSION(name, filepath) ::Atlas::Instrumentor::Get().BeginSession(name, filepath)
 	#define ATLAS_PROFILE_END_SESSION() ::Atlas::Instrumentor::Get().EndSession()
 	#define ATLAS_PROFILE_SCOPE(name) ::Atlas::InstrumentationTimer timer##__LINE__(name)
-	#define ATLAS_PROFILE_FUNCTION() ATLAS_PROFILE_SCOPE(__FUNCSIG__)
+	#define ATLAS_PROFILE_FUNCTION() ATLAS_PROFILE_SCOPE(ATLAS_FUNC_SIG)
 #else
 	#define ATLAS_PROFILE_BEGIN_SESSION(name, filepath)
 	#define ATLAS_PROFILE_END_SESSION()
