82c1103f9549a86cdd5211136746c928efc96614
Billy C. <clercbilly@gmail.com>
2024-10-24 09:53:16 +0200

    Added tangent and bitangent to meshes

--- Atlas/src/Atlas/Renderer/Mesh.h
+++ Atlas/src/Atlas/Renderer/Mesh.h
@@ -18,11 +18,19 @@ namespace Atlas
 			glm::vec3 Position;
 			glm::vec3 Normal;
 			glm::vec2 TexCoords;
+			glm::vec3 Tangent;
+			glm::vec3 Bitangent;
 
-			Vertex(float posX, float posY, float posZ, float normX, float normY, float normZ, float texU, float texV)
-				: Position(glm::vec3(posX, posY, posZ)), Normal(glm::vec3(normX, normY, normZ)), TexCoords(glm::vec2(texU, texV)) {}
-			Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 texCoords)
-				: Position(position), Normal(normal), TexCoords(texCoords) {}
+			Vertex(float posX, float posY, float posZ, float normX, float normY, float normZ, float texU, float texV,
+				float tanX = 1.0f, float tanY = 0.0f, float tanZ = 0.0f, float bitanX = 0.0f, float bitanY = 1.0f, float bitanZ = 0.0f)
+				: Position({ posX, posY, posZ }), Normal({ normX, normY, normZ }), TexCoords({ texU, texV }),
+				  Tangent({tanX, tanY, tanZ}), Bitangent({ bitanX, bitanY, bitanZ })
+			{}
+			Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 texCoords,
+				glm::vec3 tangent = { 1.0f, 0.0f, 0.0f }, glm::vec3 bitangent = { 0.0f, 1.0f, 0.0f })
+				: Position(position), Normal(normal), TexCoords(texCoords),
+				Tangent(tangent), Bitangent(bitangent)
+			{}
 		};
 
 		Mesh() { SetMeshPreset(MeshPresets::Square); }
@@ -38,6 +46,9 @@ namespace Atlas
 		const MeshPresets& GetMeshPreset() { return m_MeshPreset; }
 
 	private:
+		void CalculateSquareVertices();
+		void CalculateTangents();
+
 		MeshPresets m_MeshPreset;
 
 		std::vector<Vertex> m_Vertices;
