cb94823d4533c4a219c94597612c159d549bd3fe
Billy C. <clercbilly@gmail.com>
2024-10-28 18:03:36 +0100

    PBR Shader Prep

--- Atlas-Editor/assets/shaders/PostProcessing/PP_Frag_PBRDeferredLighting.glsl
+++ Atlas-Editor/assets/shaders/PostProcessing/PP_Frag_PBRDeferredLighting.glsl
@@ -73,7 +73,7 @@ layout (location = 1) out vec4 o_BrightColor;
 /* ----- METHOD DEFINITIONS ----- */
 /* ------------------------------ */
 
-vec3 CalculateLights(vec3 vertexPosition, vec3 vertexNormal, vec3 albedo, vec3 specular, float shininess, float ambientOcclusion);
+vec4 CalculateBrightColor(vec3 fragmentColor);
 
 /* ------------------------------ */
 /* ------------ MAIN ------------ */
@@ -81,125 +81,34 @@ vec3 CalculateLights(vec3 vertexPosition, vec3 vertexNormal, vec3 albedo, vec3 s
 
 void main()
 {
-	vec3 position          = texture(u_ScreenTextures[0], v_TexCoords).rgb;
-	vec3 normal            = texture(u_ScreenTextures[1], v_TexCoords).rgb;
-	vec3 albedo            = texture(u_ScreenTextures[2], v_TexCoords).rgb;
-	vec3 specular          = texture(u_ScreenTextures[3], v_TexCoords).rgb;
-	float shininess        = texture(u_ScreenTextures[3], v_TexCoords).a;
-	float ambientOcclusion = texture(u_ScreenTextures[4], v_TexCoords).r;
+	vec3  position         = texture(u_ScreenTextures[0], v_TexCoords).rgb;
+	vec3  normal           = texture(u_ScreenTextures[1], v_TexCoords).rgb;
+	vec3  albedo           = texture(u_ScreenTextures[2], v_TexCoords).rgb;
+	float metallic         = texture(u_ScreenTextures[3], v_TexCoords).x;
+	float roughness        = texture(u_ScreenTextures[3], v_TexCoords).y;
+	float ambientOcclusion = texture(u_ScreenTextures[4], v_TexCoords).z;
 
-	vec3 fragmentColor = CalculateLights(position, normal, albedo, specular, shininess, ambientOcclusion);
+	vec3 viewDirection  = normalize(u_CameraPosition.xyz - position);
 
-	float brightness = dot(fragmentColor.rgb, vec3(0.2126, 0.7152, 0.0722));
-	vec4 brightColor;
-	if(brightness > u_BloomThreshold)
-	{
-        brightColor = vec4(fragmentColor.rgb, 1.0);
-	}
-    else
-	{
-        brightColor = vec4(0.0, 0.0, 0.0, 1.0);
-	}
+	vec3 fragmentColor = vec3(1.0);
 
 	o_Color = vec4(fragmentColor.rgb, 1.0);
-	o_BrightColor = brightColor;
+	o_BrightColor = CalculateBrightColor(fragmentColor);
 }
 
 /* ------------------------------ */
 /* --- METHOD IMPLEMENTATIONS --- */
 /* ------------------------------ */
 
-vec3 CalculateAmbientLight(vec3 albedo, vec3 lightColor, float lightAmbientStrength, float ambientOcclusion)
+vec4 CalculateBrightColor(vec3 fragmentColor)
 {
-	return lightColor * lightAmbientStrength * albedo * ambientOcclusion;
-}
+	vec4 brightColor = vec4(0.0, 0.0, 0.0, 1.0);
 
-vec3 CalculateDiffuseLight(vec3 albedo, vec3 lightColor, float lightDiffuseStrength, vec3 lightDirection, vec3 vertexNormal)
-{
-	float diffuseImpact = max(dot(vertexNormal, lightDirection), 0.0);
-	return lightColor * lightDiffuseStrength * diffuseImpact * albedo;
-}
-
-vec3 CalculateSpecularLight(vec3 specular, vec3 lightColor, float lightSpecularStrength, vec3 lightDirection, vec3 vertexNormal, vec3 vertexPosition, float shininess)
-{
-	vec3 viewDirection  = normalize(u_CameraPosition.xyz - vertexPosition);
-	
-	/* --- Phong Specular Lighting  --- */ 
-	//vec3 reflectionDirection = reflect(-lightDirection, vertexNormal);
-	//float specularFactor     = pow(max(dot(viewDirection, reflectionDirection), 0.0), VertexInput.Shininess == 0 ? 1 : VertexInput.Shininess * 128); // TODO: Fix Shininess == 0
-
-	/* --- Blinn-Phong Specular Lighting  --- */
-	vec3 halfwayDirection = normalize(lightDirection + viewDirection);
-	float specularFactor  = pow(max(dot(vertexNormal, halfwayDirection), 0.0), shininess * 128);
-
-	return lightColor * lightSpecularStrength * (specularFactor * specular);
-}
-
-float CalculateLightAttenuation(float lightRadius, vec3 lightPosition, vec3 vertexPosition)
-{
-	float attenuation = 1.0;
-
-	if(lightRadius > 0)
+	float brightness = dot(fragmentColor.rgb, vec3(0.2126, 0.7152, 0.0722));
+	if(brightness > u_BloomThreshold)
 	{
-		float dist  = length(lightPosition - vertexPosition);
-
-		float temp  = dist * dist + lightRadius * lightRadius;
-		attenuation = 2.0 / (temp + dist * sqrt(temp));
+        brightColor = vec4(fragmentColor.rgb, 1.0);
 	}
 
-	return attenuation;
-}
-
-float CalculateLightCutOff(vec2 lightCutOff, vec3 lightDirection, vec3 spotDirection)
-{
-	float cutOff = 1.0f;
-
-	if(lightCutOff.x >= 0 && lightCutOff.y >= 0)
-	{
-		float theta   = dot(lightDirection, normalize(-spotDirection));
-		float epsilon = lightCutOff.x - lightCutOff.y;
-		cutOff        = clamp((theta - lightCutOff.y) / epsilon, 0.0, 1.0);
-	}
-
-	return cutOff;
-}
-
-vec3 CalculateLights(vec3 vertexPosition, vec3 vertexNormal, vec3 albedo, vec3 specular, float shininess, float ambientOcclusion)
-{
-	vec3 ambientColor  = vec3(0.0);
-	vec3 diffuseColor  = vec3(0.0);
-	vec3 specularColor = vec3(0.0);
-
-	for (uint lightIndex = 0; lightIndex < u_LightCount; lightIndex++)
-	{
-		LightData light = u_Lights[lightIndex];
-
-		vec3 lightColor = pow(light.Color.rgb, vec3(u_Gamma)) * light.Intensity;
-
-		vec3 lightDirection;
-		if(light.Direction.w == 0)
-		{
-			lightDirection = normalize(light.Position.xyz - vertexPosition);
-		}
-		else
-		{
-			lightDirection = normalize(-light.Direction.xyz); // For directional light ; light direction is spot direction
-		}
-		
-		float attenuation = CalculateLightAttenuation(light.Radius, light.Position.xyz, vertexPosition);
-		float lightCutOff = CalculateLightCutOff(light.CutOffs, normalize(light.Position.xyz - vertexPosition), light.Direction.xyz);
-
-		vec3 ambientLight  = CalculateAmbientLight (albedo,   lightColor, light.AmbientStrength                                               , ambientOcclusion);
-		vec3 diffuseLight  = CalculateDiffuseLight (albedo,   lightColor, light.DiffuseStrength , lightDirection, vertexNormal                                  );
-		vec3 specularLight = CalculateSpecularLight(specular, lightColor, light.SpecularStrength, lightDirection, vertexNormal, vertexPosition, shininess       );
-		ambientLight  *= attenuation;
-		diffuseLight  *= attenuation * lightCutOff;
-		specularLight *= attenuation * lightCutOff;
-
-		ambientColor  += ambientLight;
-		diffuseColor  += diffuseLight;
-		specularColor += specularLight;
-	}
-
-	return ambientColor + diffuseColor + specularColor;
+	return brightColor;
 }
